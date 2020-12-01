961dc205dc4fce79bf6ab9792470654a05923471
dolapo <dolapo96@gmail.com>
2020-12-01 13:35:21 +0000

    Added Data Annotation to Models and setup react environment

--- MvpStore/Models/Customer.cs
+++ MvpStore/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvpStore.Models
 {
@@ -11,7 +12,13 @@ namespace MvpStore.Models
         }
 
         public int Id { get; set; }
+
+        [StringLength(50), MinLength(3)]
+        [Required(ErrorMessage = "Customer Name is required")]
         public string Name { get; set; }
+
+        [StringLength(50), MinLength(3)]
+        [Required(ErrorMessage = "Customer Address is required")]
         public string Address { get; set; }
 
         public virtual ICollection<Sales> Sales { get; set; }
--- MvpStore/Models/Product.cs
+++ MvpStore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvpStore.Models
 {
@@ -11,7 +12,13 @@ namespace MvpStore.Models
         }
 
         public int Id { get; set; }
+
+        [StringLength(50), MinLength(3)]
+        [Required(ErrorMessage = "Product Name is required")]
         public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Required(ErrorMessage = "Price is required")]
         public decimal Price { get; set; }
 
         public virtual ICollection<Sales> Sales { get; set; }
